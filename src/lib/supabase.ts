12751989602ea901cf8d5efd81b5fd113fcc65c6
thwin khant oo <thwinkhant138@gmail.com>
2026-07-14 01:22:31 +0630

    updating supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://kitrjktrnrtnpaazkegx.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpdHJqa3RybnJ0bnBhYXprZWd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NDM3NzIsImV4cCI6MjA3ODMxOTc3Mn0.n1bhj3AILZQ6I7bkStsZmRik0Ush9fnwttGciLuf1yc'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://jhwfjntomkgfjnnefhxf.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impod2ZqbnRvbWtnZmpubmVmaHhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMDc5OTQsImV4cCI6MjA4ODg4Mzk5NH0.Y_l8FfAq7m_RGNL03gxdvocJOYVFTLhMWxopvVhAmJs'
 
 // Client-side Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
